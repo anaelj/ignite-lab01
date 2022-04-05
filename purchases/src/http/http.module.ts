50af37d74cd5e23777ef29cdb88cc9b577b42895
Anael Medeiros <anael.medeiros@compasso.com.br>
2022-04-04 20:30:33 -0400

    add project

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '../database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
+import { ApolloDriver } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.qql'),
+    }),
+  ],
+  //   controllers: [],
+  //   providers: [],
+})
+export class HttpModule {}
---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,16 @@
+import { getSession, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+
+export default function Home() {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <Link href={`/api/auth/logout`}>Logout</Link>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired();
